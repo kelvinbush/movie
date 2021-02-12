4d6fb16fb3fb66e915c5c6a5b1c337f96254bba8
Kelvin Bush <kelybush@gmail.com>
2021-02-12 21:14:08 +0300

    add episodes
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

---
+++ app/src/main/java/com/kelvinwachiye/kotlin/moviedb/domains/Season.kt
@@ -0,0 +1,13 @@
+package com.kelvinwachiye.kotlin.moviedb.domains
+
+data class Season(
+    val episodes: List<Episode>?,
+    val season_number: Int?
+)
+
+data class Episode(
+    val name: String?,
+    val air_date: String?,
+    val overview: String?,
+    val still_path: String?
+)
