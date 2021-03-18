0fe45fac4255afac9f336c8d634a23764388d3ba
Kelvin Bush <kelybush@gmail.com>
2021-03-18 18:54:07 +0300

    use binding in episodes adapter
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/src/main/java/com/kelvinwachiye/kotlin/moviedb/adapters/EpisodesAdapter.kt
+++ app/src/main/java/com/kelvinwachiye/kotlin/moviedb/adapters/EpisodesAdapter.kt
@@ -1,19 +1,14 @@
 package com.kelvinwachiye.kotlin.moviedb.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.kelvinwachiye.kotlin.moviedb.R
+import com.kelvinwachiye.kotlin.moviedb.databinding.EpisodesListItemBinding
 import com.kelvinwachiye.kotlin.moviedb.domains.Episode
 
 class EpisodesAdapter : ListAdapter<Episode, EpisodesAdapter.ViewHolder>(EpisodesDiffCallBack()) {
-
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
@@ -25,21 +20,21 @@ class EpisodesAdapter : ListAdapter<Episode, EpisodesAdapter.ViewHolder>(Episode
     }
 
 
-    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val overview: TextView = itemView.findViewById(R.id.overview)
-        val stillPath: ImageView = itemView.findViewById(R.id.stillPath)
-        val name: TextView = itemView.findViewById(R.id.name)
+    class ViewHolder private constructor(val binding: EpisodesListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: Episode) {
-            name.text = item.name
-            overview.text = item.overview
+            binding.apply {
+                name.text = item.name
+                overview.text = item.overview
+            }
         }
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.episodes_list_item, parent, false)
-                return ViewHolder(view)
+                val binding = EpisodesListItemBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
     }
