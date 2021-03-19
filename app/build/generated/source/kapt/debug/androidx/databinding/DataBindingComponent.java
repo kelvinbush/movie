cedc9a77b57000ec5271fcf282ff5242b8355ea5
Kelvin Bush <kelybush@gmail.com>
2021-03-19 11:24:09 +0300

    refactor textviews
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- app/build/generated/source/kapt/debug/androidx/databinding/DataBindingComponent.java
+++ app/build/generated/source/kapt/debug/androidx/databinding/DataBindingComponent.java
@@ -1,5 +1,4 @@
 package androidx.databinding;
 
 public interface DataBindingComponent {
-    com.kelvinwachiye.kotlin.moviedb.ui.showdetails.CastBindingUtils getCastBindingUtils();
 }
--- app/src/main/java/com/kelvinwachiye/kotlin/moviedb/ui/showdetails/BindingUtils.kt
+++ app/src/main/java/com/kelvinwachiye/kotlin/moviedb/ui/showdetails/BindingUtils.kt
@@ -7,24 +7,8 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.kelvinwachiye.kotlin.moviedb.R
 import com.kelvinwachiye.kotlin.moviedb.constants.MyConstants
-import com.kelvinwachiye.kotlin.moviedb.domains.Cast
-import com.kelvinwachiye.kotlin.moviedb.domains.Episode
 
-@BindingAdapter("character")
-fun TextView.setCharacter(item: Cast?) {
-    item?.let {
-        text = item.character
-    }
-}
-
-@BindingAdapter("originalName")
-fun TextView.setOriginalName(item: Cast?) {
-    item?.let {
-        text = item.original_name
-    }
-}
-
-@BindingAdapter("castImage")
+@BindingAdapter("imageUrl")
 fun setCastImage(view: ImageView, url: String) {
     Glide.with(view.context)
         .load(MyConstants.IMAGE_BASE_URL + url)
@@ -33,16 +17,7 @@ fun setCastImage(view: ImageView, url: String) {
 
 }
 
-@BindingAdapter("overview")
-fun TextView.setOverview(item: Episode?) {
-    item?.let {
-        text = item.overview
-    }
-}
-
-@BindingAdapter("name")
-fun TextView.setName(item: Episode?) {
-    item?.let {
-        text = item.name
-    }
+@BindingAdapter("textHere")
+fun setTextViewText(view: TextView, textPassed: String?) {
+    view.text = textPassed
 }
